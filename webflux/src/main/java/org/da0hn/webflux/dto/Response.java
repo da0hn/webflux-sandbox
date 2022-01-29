5e41be90c46385a4102d1fbb2efd2a128be02a3e
Gabriel Honda <gabrielhondacba@gmail.com>
2022-01-29 11:04:07 -0400

    test: add request using `synchronous` GET method

--- webflux/src/main/java/org/da0hn/webflux/dto/Response.java
+++ webflux/src/main/java/org/da0hn/webflux/dto/Response.java
@@ -2,17 +2,19 @@ package org.da0hn.webflux.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @ToString
+@NoArgsConstructor
 public class Response {
 
   @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss.SSSS")
   private final LocalDateTime date = LocalDateTime.now();
-  private final int output;
+  private int output;
 
 
   public Response(final int output) {
--- webflux/src/test/java/org/da0hn/webflux/GetSingleResponseTest.java
+++ webflux/src/test/java/org/da0hn/webflux/GetSingleResponseTest.java
@@ -1,12 +1,31 @@
 package org.da0hn.webflux;
 
+import org.da0hn.webflux.dto.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GetSingleResponseTest extends BaseTest {
 
   @Autowired
   private WebClient webClient;
+
+  @Test
+  @DisplayName("Blocking endpoint call")
+  void test1() {
+    final var response = this.webClient.get()
+      .uri(builder -> builder.path("/asynchronous-math/square")
+        .queryParam("input", 10)
+        .build()
+      )
+      .retrieve()
+      .bodyToMono(Response.class) // Mono<Response>
+      .block();// Blocking operation
+    final var EXPECTED = 100;
+    assertEquals(EXPECTED, response.getOutput());
+  }
 
 
 }
