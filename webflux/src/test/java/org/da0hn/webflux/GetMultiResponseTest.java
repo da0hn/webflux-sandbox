a391c30c5160f0df4a0386aead3fe28cff37c8ee
Gabriel Honda <gabrielhondacba@gmail.com>
2022-01-29 11:17:23 -0400

    test: add request for List using `asynchronous` GET method

---
+++ webflux/src/test/java/org/da0hn/webflux/GetMultiResponseTest.java
@@ -0,0 +1,34 @@
+package org.da0hn.webflux;
+
+import org.da0hn.webflux.dto.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+class GetMultiResponseTest extends BaseTest {
+
+  @Autowired
+  private WebClient webClient;
+
+
+  @Test
+  @DisplayName("Non blocking get list")
+  void test1() {
+    final var response = this.webClient.get()
+      .uri(builder -> builder.path("/asynchronous-math/table")
+        .queryParam("input", 10)
+        .build()
+      )
+      .retrieve()
+      .bodyToFlux(Response.class) // Flux<Response>
+      .doOnNext(System.out::println);
+
+    StepVerifier.create(response)
+      .expectNextCount(10)
+      .verifyComplete();
+  }
+
+
+}
