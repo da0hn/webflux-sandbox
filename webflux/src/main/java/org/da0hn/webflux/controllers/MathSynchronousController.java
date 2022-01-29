e666e3cf56d90a119f54ebe730c940dec7b768ae
Gabriel Honda <gabrielhondacba@gmail.com>
2022-01-28 21:59:30 -0400

    feat: add synchronous controller example

---
+++ webflux/src/main/java/org/da0hn/webflux/controllers/MathSynchronousController.java
@@ -0,0 +1,36 @@
+package org.da0hn.webflux.controllers;
+
+import org.da0hn.webflux.dto.Response;
+import org.da0hn.webflux.services.MathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/math")
+public class MathSynchronousController {
+
+  private final MathService mathService;
+
+
+  @Autowired
+  public MathSynchronousController(final MathService mathService) {
+    this.mathService = mathService;
+  }
+
+  @GetMapping("/square")
+  public Response findSquare(@RequestParam final int input) {
+    return this.mathService.findSquare(input);
+  }
+
+  @GetMapping("/table")
+  public List<Response> multiplicationTable(@RequestParam final int input) {
+    return this.mathService.multiplicationTable(input);
+  }
+
+
+}
