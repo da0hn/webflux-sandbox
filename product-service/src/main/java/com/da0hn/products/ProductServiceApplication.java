89b73215c3609c91e9bef66f4c5eba8b4abb264e
Gabriel Honda <gabrielhondacba@gmail.com>
2022-01-30 11:37:15 -0400

    chore: setup product microservice

---
+++ product-service/src/main/java/com/da0hn/products/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.da0hn.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(ProductServiceApplication.class, args);
+  }
+
+}
---
+++ product-service/src/test/java/com/da0hn/products/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da0hn.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
