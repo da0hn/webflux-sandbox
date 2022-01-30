0d479f5725fa925af3ddfffc9bac0e23b5c12ee8
Gabriel Honda <gabrielhondacba@gmail.com>
2022-01-30 11:44:18 -0400

    feat: add reactive product repository

---
+++ product-service/src/main/java/com/da0hn/products/data/ReactiveMongoProductRepository.java
@@ -0,0 +1,9 @@
+package com.da0hn.products.data;
+
+import com.da0hn.products.core.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReactiveMongoProductRepository extends ReactiveMongoRepository<Product, String> {
+}
