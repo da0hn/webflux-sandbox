4f26eb51a487d77bf2640aeb455197635a60bc6b
Gabriel Honda <gabrielhondacba@gmail.com>
2022-02-03 21:21:24 -0400

    chore: setup user microservice

---
+++ user-service/src/test/java/com/da0hn/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da0hn.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
