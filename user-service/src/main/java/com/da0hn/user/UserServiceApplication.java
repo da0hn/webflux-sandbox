aa3e1a71952e223e2b782bd1096cac39e080b9da
Gabriel Honda <gabrielhondacba@gmail.com>
2022-02-03 21:25:00 -0400

    feat: add request and response `User` objects

--- user-service/src/main/java/com/da0hn/user/UserServiceApplication.java
+++ user-service/src/main/java/com/da0hn/user/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
   }
 
---
+++ user-service/src/main/java/com/da0hn/user/application/dtos/UserResponse.java
@@ -0,0 +1,4 @@
+package com.da0hn.user.application.dtos;
+
+public record UserResponse(Long id, String name, Double balance) {
+}
