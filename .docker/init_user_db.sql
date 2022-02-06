3c4e8ab134e76d4016beb602df83b170b0df04d4
da0hn <gabrielhondacba@gmail.com>
2022-02-06 10:34:18 -0400

    chore: add script to initialize database

---
+++ .docker/init_user_db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users
+(
+    id      serial
+        constraint pk_id_user primary key,
+    name    varchar(50),
+    balance double precision
+);
+
+CREATE TABLE transactions
+(
+    id      serial
+        constraint pk_id_transaction primary key,
+    user_id bigint,
+    amount  double precision,
+    date    timestamp,
+    foreign key (user_id) references users (id)
+);
+
