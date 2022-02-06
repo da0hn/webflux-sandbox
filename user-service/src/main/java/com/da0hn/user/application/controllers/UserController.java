8dbfd9e4b58e07cc0ff60fd3757f6fb24c87b64d
da0hn <gabrielhondacba@gmail.com>
2022-02-06 10:21:37 -0400

    feat: add CRUD operations of `User` in endpoints

---
+++ user-service/src/main/java/com/da0hn/user/application/controllers/UserController.java
@@ -0,0 +1,73 @@
+package com.da0hn.user.application.controllers;
+
+import com.da0hn.user.application.dtos.UserRequest;
+import com.da0hn.user.application.dtos.UserResponse;
+import com.da0hn.user.core.usecases.user.CreateUser;
+import com.da0hn.user.core.usecases.user.DeleteUserById;
+import com.da0hn.user.core.usecases.user.GetAllUsers;
+import com.da0hn.user.core.usecases.user.GetUserById;
+import com.da0hn.user.core.usecases.user.UpdateUser;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+  private final CreateUser createUser;
+  private final DeleteUserById deleteUserById;
+  private final GetAllUsers getAllUsers;
+  private final GetUserById getUserById;
+  private final UpdateUser updateUser;
+
+  @GetMapping
+  public Flux<ResponseEntity<UserResponse>> getAll() {
+    return this.getAllUsers.execute()
+      .map(ResponseEntity::ok)
+      .defaultIfEmpty(ResponseEntity.noContent().build());
+  }
+
+  @PostMapping
+  public Mono<ResponseEntity<UserResponse>> create(@RequestBody final Mono<UserRequest> request) {
+    return this.createUser.execute(request)
+      .map(ResponseEntity::ok)
+      .defaultIfEmpty(ResponseEntity.badRequest().build());
+  }
+
+  @GetMapping("/{id-user}")
+  public Mono<ResponseEntity<UserResponse>> getById(@RequestParam("id-user") final Long idUser) {
+    return this.getUserById.execute(idUser)
+      .map(ResponseEntity::ok)
+      .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/{id-user}")
+  public Mono<ResponseEntity<UserResponse>> update(
+    @RequestBody final Mono<UserRequest> request,
+    @RequestParam("id-user") final Long idUser
+  ) {
+    return this.updateUser.execute(request, idUser)
+      .map(ResponseEntity::ok)
+      .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/{id-user}")
+  public Mono<ResponseEntity<Object>> deleteById(@RequestParam("id-user") final Long idUser) {
+    return this.deleteUserById.execute(idUser)
+      .map(e -> ResponseEntity.noContent().build())
+      .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
+
+
+}
