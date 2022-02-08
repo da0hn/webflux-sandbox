dce3e389fc520486cc0188f08d2d1bab5d8bb91f
da0hn <gabrielhondacba@gmail.com>
2022-02-07 20:24:04 -0400

    chore: setup `order` microservice

---
+++ order-service/src/test/java/org/da0hn/orders/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.da0hn.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
